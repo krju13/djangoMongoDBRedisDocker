14ecbfc306936b55117619b79dc427130ff0c5c0
krju13 <krju13@gmail.com>
2021-12-17 16:45:55 +0900

    static file config

--- djangomongoredis/djangomongoredis/settings.py
+++ djangomongoredis/djangomongoredis/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,9 +137,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+ROOT_DIR=os.path.dirname(BASE_DIR)
 STATIC_URL = '/static/'
-
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[
+    STATIC_DIR,
+]
+STATIC_ROOT=os.path.join(ROOT_DIR,'.static_root')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
